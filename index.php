2cb9327472cd0aaf73ed582aa77b4495b7e3928c
Roman Nahornyi <nahornyi.roman@gmail.com>
2020-07-30 20:36:47 +0300

    composer dep

--- index.php
+++ index.php
@@ -20,12 +20,15 @@ define('MYSQL_DEBUG', false);
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
-define('PROJECT_DIR', dirname(__DIR__));
+define('PROJECT_DIR', __DIR__);
 
-/* Including packages from the dashboard project */
-require_once __DIR__ . '/../vendor/autoload.php';
+/* Including packages from the composer dependencies */
+require_once __DIR__ . '/vendor/autoload.php';
+
+/* Including packages from the old vendor */
+require_once __DIR__ . '/vendor-static/autoload.php';
 
 /* Loading environment variables */
-\Dotenv\Dotenv::create(dirname(__DIR__), '.env')->load();
+\Dotenv\Dotenv::create(__DIR__, '.env')->load();
 
 require 'core/base.php';
