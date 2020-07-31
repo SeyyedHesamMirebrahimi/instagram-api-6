64d6784ede93af7e9af0072391f2e53492b7447a
Roman Nahornyi <nahornyi.roman@gmail.com>
2020-07-31 20:48:30 +0300

    Update env. variable

--- core/config/config.php
+++ core/config/config.php
@@ -5,7 +5,7 @@ $config = [
     'database_host'        => getenv('MYSQL_HOST'),
     'database_username'    => getenv('MYSQL_USER'),
     'database_password'    => getenv('MYSQL_PASSWORD'),
-    'database_name'        => getenv('MYSQL_PRODUCT_DATABASE'),
+    'database_name'        => getenv('MYSQL_DATABASE'),
     'database_port'        => getenv('MYSQL_PORT'),
     'url'                  => getenv('PRODUCT_URL')
 ];
